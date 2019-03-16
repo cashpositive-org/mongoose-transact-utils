9f45a6f7e362f2cd3e742f79047b27510d1157b4
Soumyajit Pathak <s.pathak5995@gmail.com>
2019-03-16 17:18:46 +0530

    Initial commit & build tools setup (#1)

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import mongoose, { ClientSession } from 'mongoose';
+
+export type MutationCallback<T> = (session: ClientSession) => Promise<T>;
+
+/**
+ * Runs the provided `mutations` callback within a transaction and commits the changes to the DB
+ * only when it has run successfully.
+ *
+ * @param mutations A callback which does DB writes and reads using the session.
+ */
+export async function runInTransaction<T>(mutations: MutationCallback<T>): Promise<T> {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const value = await mutations(session);
+
+    // Since the mutations ran without an error, commit the transaction.
+    await session.commitTransaction();
+
+    // Return any value returned by `mutations` to make this function as transparent as possible.
+    return value;
+  } catch (error) {
+    // Abort the transaction as an error has occurred in the mutations above.
+    await session.abortTransaction();
+
+    // Rethrow the error to be caught by the caller.
+    throw error;
+  } finally {
+    // End the previous session.
+    session.endSession();
+  }
+}
