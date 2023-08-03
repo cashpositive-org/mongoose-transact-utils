217f88a3101809bfd0d088499d0cbf8daa330c0b
dennis <dennis@movesfinancial.com>
2023-08-03 15:00:01 -0400

    Chore: clean up library and bump version

--- docs/assets/js/search.js
+++
@@ -1,3 +0,0 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"64":"Function","65536":"Type literal","4194304":"Type alias"},"rows":[{"id":0,"kind":4194304,"name":"MutationCallback","url":"globals.html#mutationcallback","classes":"tsd-kind-type-alias tsd-has-type-parameter"},{"id":1,"kind":65536,"name":"__type","url":"globals.html#mutationcallback.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported","parent":"MutationCallback"},{"id":2,"kind":64,"name":"runInTransaction","url":"globals.html#runintransaction","classes":"tsd-kind-function tsd-has-type-parameter"}]};
